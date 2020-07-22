355d8b05a1ab8b6b4c1fa231c8f15a19c653b0e2
Denis <Dennicraft.Ds@gmail.com>
2020-07-22 19:17:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//  alert('Hello');
+
+// const result = confirm("Are your here");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?","18");
+// console.log(answer+5);
+
+const answers = [];
+
+// answers[0] = prompt("Name?", "");
+// answers[1] = prompt("Surname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+console.log(typeof(answers));
+console.log(typeof(null));
